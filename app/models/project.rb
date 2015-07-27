d351719ca277729ea46a8cd5cb766076d780b4f1
Eli <55drk1@gmail.com>
2015-07-27 17:14:42 +0200

    Changin model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,7 @@
 class Project < ActiveRecord::Base
+
+	def iron_find(id)
+		where(id: id).first
+	end
+
 end
---
+++ db/migrate/20150727133550_add_descrption_to_projects.rb
@@ -0,0 +1,5 @@
+class AddDescrptionToProjects < ActiveRecord::Migration
+  def change
+    add_column :projects, :description, :text
+  end
+end
