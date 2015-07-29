fb6fce78acc8388742678d7061ba4a4db4e6236c
Eli <55drk1@gmail.com>
2015-07-29 10:49:45 +0200

    Validating atributes

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,7 @@
 class Entry < ActiveRecord::Base
 	belongs_to :project
+	validates :hours, numericality: {only_integer: true, greather_than: 0}, presence: true
+	validates :minutes, numericality: {only_integer: true}, presence: true
+	validates :datetime, presence: true
+	validates :project, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,3 @@
-Project.create name: 'Ironhack', description: 'Ironhack is a s....'
-Project.create name: 'Time tracking app'
-Project.create name: 'Recipes', description: 'Track my favourite Recipes'
-
-
 25.times do |i|
 	
 	project = Project.create name: "Name#{i}"  , description: 'ttttt'
